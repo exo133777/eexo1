71ea6da973b64b5128d558671565e36d6cb5ee99
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-05-15 18:20:38 +0000

    Initial commit

---
+++ src/pages/Copy 3 of Home.ja2la.js
@@ -0,0 +1,26 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+	// Write your Javascript code here using the Velo framework API
+
+	// Print hello world:
+	// console.log("Hello world!");
+
+	// Call functions on page elements, e.g.:
+	// $w("#button1").label = "Click me!";
+
+	// Click "Run", or Preview your site, to execute your code
+
+
+
+
+/**
+*	Adds an event handler that runs when the element is clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function form1_click(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
